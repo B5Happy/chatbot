f02ef8b4990b86eac7d913daceb91171ef900460
Saly <saly@juke4me.com>
2018-12-12 08:49:36 +0530

    update

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const rl = Readline.createInterface({
     output: process.stdout,
     terminal: false
 })
-
+const matcher = require('./matcher');
 rl.setPrompt('> ');
 rl.prompt();
 rl.on('line', reply => {
-    console.log(`You said ${reply}`);
-    rl.prompt();
+    matcher(reply, data => {
+        switch (data.intent) {
+            case 'Hello':
+                console.log("Helloooo")
+                rl.prompt();
+                break;
+            case 'Exit':
+                console.log("Bye, have a great day!");
+                process.exit(0);
+                break;
+            default: {
+                console.log("I don't know what you mean :/");
+                rl.prompt();
+            }
+        }
+    })
 })
